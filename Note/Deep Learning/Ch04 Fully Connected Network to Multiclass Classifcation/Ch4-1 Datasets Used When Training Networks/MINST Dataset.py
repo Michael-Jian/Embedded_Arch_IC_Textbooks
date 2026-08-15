bb3b5aa7f3479dc4ffde71b05987a49ad4f6ac7a
Michael Jian <michael21314@gmail.com>
2026-08-16 00:19:13 +0800

    edit code

--- Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/MINST Dataset.py
+++ Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/MINST Dataset.py
@@ -1,31 +1,33 @@
+
+# 
 import idx2numpy
 
 # Insert files path : 
-TRAIN_IMAGE_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/train-images.idx3-ubyte'
-TRAIN_LABEL_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/train-labels.idx1-ubyte'
+TRAINING_IMAGE_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/train-images.idx3-ubyte'
+TRAINING_LABEL_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/train-labels.idx1-ubyte'
 TEST_IMAGE_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/t10k-images.idx3-ubyte'
 TEST_LABEL_FILENAME = '/home/michael/Embedded_Arch_IC_Textbooks/Note/Deep Learning/Ch04 Fully Connected Network to Multiclass Classifcation/Ch4-1 Datasets Used When Training Networks/mnist dataset/t10k-labels.idx1-ubyte'
 
 # Read files : 
-# idx2numpy.convert_from_file( filename ) : convert specific format file into the format fit for matrix multiplication.
-train_images = idx2numpy.convert_from_file( TRAIN_IMAGE_FILENAME )
-train_labels = idx2numpy.convert_from_file( TRAIN_LABEL_FILENAME )
+# idx2numpy.convert_from_file( filename ) : convert specific format file into NumPy 3D array format to fit for matrix multiplication.
+training_images = idx2numpy.convert_from_file( TRAINING_IMAGE_FILENAME )
+training_labels = idx2numpy.convert_from_file( TRAINING_LABEL_FILENAME )
 test_images = idx2numpy.convert_from_file( TEST_IMAGE_FILENAME )
 test_labels = idx2numpy.convert_from_file( TEST_LABEL_FILENAME )
 
 # Print number and dimension : 
 # filename.shape : introduce the number of data and dimension of data in this file.
-print( 'number and dimension of training images : ' , train_images.shape ) # 6000 images and 28 * 28 pixel of each.
-print( 'number and dimension of training labels : ' , train_labels.shape ) # 6000 answers and no pixel of each.
+print( 'number and dimension of training images : ' , training_images.shape ) # 6000 images and 28 * 28 pixel of each.
+print( 'number and dimension of training labels : ' , training_labels.shape ) # 6000 answers and no pixel of each.
 print( 'number and dimension of test images : ' , test_images.shape ) # 10000 images and 28 * 28 pixel of each.
 print( 'number and dimension of test labels : ' , test_labels.shape ) # 10000 answers and no pixel of each.
 
 # Print one training example : 
-print( 'first training labels : ' , train_labels[ 0 ] )
+print( 'first training labels : ' , training_labels[ 0 ] )
 print( 'first training images : ' )
 # training images in train_images is not traditonal format(.png/.jpeg ) but binary format (.idx-ubyte ),
 #so we use specific method to show it below.
-for row in train_images[ 0 ] :
+for row in training_images[ 0 ] :
     for num in row :
         if num == 0 : 
             # print( 'x' , end = 'y' ) : after printing x , print "y" consecutively and don't change to next row.
